0718491628aa48675779ccda9d7ba3d049e3e476
Porndet Pipitsuntonsan <likeink2@hotmail.com>
2023-02-18 21:13:02 +0900

    new commit

--- Fishing_Rox.py
+++ Fishing_Rox.py
@@ -4,8 +4,8 @@ from win32gui import FindWindow, GetWindowRect
 Total_Fishing, Location_Fishing, i = Total_Location()
 Noxplayer = FindWindow(None, "NoxPlayer")
 left, top, width, height  = GetWindowRect(Noxplayer)
-# print(left, top, width, height)
-while i < int(Total_Fishing):
-    Center_iconX, Center_iconY = Fising_Icon(top, left, width - left, height - top)
-    if(Fishing_Click(int(Location_Fishing), int(Center_iconX), int(Center_iconY - 54)) == True):
-        i += 1
+print(left, top, width, height)
+# while i < int(Total_Fishing):
+#     Center_iconX, Center_iconY = Fising_Icon(top, left, width - left, height - top)
+#     if(Fishing_Click(int(Location_Fishing), int(Center_iconX), int(Center_iconY - 54)) == True):
+#         i += 1
